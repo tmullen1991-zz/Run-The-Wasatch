7878edc12e989f9e9af0ca29f9ac8db02293e13b
Abram Wingle <abramwingle15@gmail.com>
2019-04-20 12:57:34 -0600

    auth0 routes being configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React, { Component } from 'react';
+import Home from "./pages/Home"
 import './App.css';
 import Auth from './Auth/Auth.js';
 
 const auth = new Auth();
-auth.login();
+//auth.login();
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        it works!!
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>it works yee yee</div>
+    )
+}
+
+export default Home;
