c41004efda85efcfa3f527dbe5b47eb897c316d2
Abram Wingle <abramwingle15@gmail.com>
2019-04-20 13:30:57 -0600

    login and redirect working

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import React, { Component } from 'react';
 import Home from "./pages/Home"
 import './App.css';
 import Auth from './Auth/Auth.js';
+import Callback from "./pages/Callback";
+import history from "./history"
 
 const auth = new Auth();
-//auth.login();
+
+if (!auth.isAuthenticated() && window.location.pathname !== "/callback"){
+  debugger;
+  auth.login();
+}
+
+
+
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
 
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <div>
           <Route exact path="/" component={Home} />
-
+          <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} /> 
+          }}/>
         </div>
       </Router>
     );
