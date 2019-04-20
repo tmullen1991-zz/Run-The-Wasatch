e2e622ee1aae02e8abb6a3b2930276aeb23f766c
Abram Wingle <abramwingle15@gmail.com>
2019-04-20 11:10:38 -0600

    auth 0

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,74 @@ import Auth from './Auth/Auth.js';
 
 const auth = new Auth();
 auth.login();
-
 class App extends Component {
-  render() {
-    return (
-      <div>
-        it works!!
-      </div>
-    );
-  }
-}
 
-export default App;
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
+
+  render() {
+
+        const { isAuthenticated } = this.props.auth;
+
+        return (
+          <div>
+            <nav>
+              <nav>
+                <nav>
+                  <a href="#">Auth0 - React</a>
+                </nav>
+                <button
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.goTo.bind(this, 'home')}
+                >
+                  Home
+                </button>
+                {
+                  !isAuthenticated() && (
+                      <button
+                        bsStyle="primary"
+                        className="btn-margin"
+                        onClick={this.login.bind(this)}
+                      >
+                        Log In
+                      </button>
+                    )
+                }
+                {
+                  isAuthenticated() && (
+                      <button
+                        bsStyle="primary"
+                        className="btn-margin"
+                        onClick={this.logout.bind(this)}
+                      >
+                        Log Out
+                      </button>
+                    )
+                }
+              </nav>
+            </nav>
+          </div>
+        );
+      }
+    }
+    
+    export default App;
---
+++ client/src/Callback/Callback.js
@@ -0,0 +1,15 @@
+// src/Callback/Callback.js
+
+import React, { Component } from 'react';
+import loading from './loading.svg';
+
+class Callback extends Component {
+  render() {
+
+    return (
+      <h2>everything is awesome</h2>
+    );
+  }
+}
+
+export default Callback;
